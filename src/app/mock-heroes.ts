38190e5cb461fc449ba55425a9654fb2f11c1477
Brandon Vergara <brandon@makingdevs.com>
2021-03-23 17:06:52 -0600

    coding main  I changed names in objects

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,15 +1,15 @@
 import {Hero}from "./hero";
 
 export const HEROES: Hero[]=[
-    {id: 11, name:"Sperman" ,edad:23, poder:"Acero",univers:"DC"},
-    {id: 12,name:"Wolverine", edad:40, poder:"Inmortal" ,univers:"MARVEL"},
+    {id: 11, name:"Superman" ,edad:23, poder:"Volar, Fuerza Sobrehumana, Telequinesis, Invisibilidad, Alas de Fuego",univers:"DC"},
+    {id: 12,name:"Wolverine", edad:40, poder:"Regeneracion, Garras Rectractiles" ,univers:"MARVEL"},
     {id: 13, name:"Hulk", edad:30, poder:"Fuerza",univers:"MARVEL"},
     {id: 14,name:"Spider-Man", edad:44,poder:"Trepar Paredes"  ,univers:"MARVEL"},
     {id: 15,name:"Batman", edad:34, poder:"Intelecto Nivel Genio" ,univers:"DC"},
-    {id: 16, name:"Flash",edad:19, poder:"Acero" ,univers:"DC"},
-    {id: 17,name:"Super Girl" ,edad:25, poder:"Acero" ,univers:"DC"},
-    {id: 18,name:"Bruja Scarlett" ,edad:34,poder:"Acero" ,univers:"MARVEL"},
-    {id: 19, name:"Woder Woman",edad:22, poder:"Acero" ,univers:"DC"},
-    {id: 20,name:"Thor" ,edad:1000, poder:"Acero" ,univers:"MARVEL"}
+    {id: 16, name:"Flash",edad:19, poder:"Super Velocidad" ,univers:"DC"},
+    {id: 17,name:"Super Girl" ,edad:25, poder:"Volar, Fuerza Sobrehumana, Telequinesis, Invisibilidad, Alas de Fuego" ,univers:"DC"},
+    {id: 18,name:"Scarlet Witch" ,edad:34,poder:"Proyección Astral, Control del Tiempo" ,univers:"MARVEL"},
+    {id: 19, name:"Woder Woman",edad:22, poder:"Diosa Griega" ,univers:"DC"},
+    {id: 20,name:"Thor" ,edad:1000, poder:"Dios Del Trueno" ,univers:"MARVEL"}
 
 ]

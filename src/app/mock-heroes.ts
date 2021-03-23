97ee08ea96b24c5fc558250f6a717be41ca99a9c
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-03-23 14:17:54 -0600

    add component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero}from "./hero";
+
+export const HEROES: Hero[]=[
+    {id: 11, name:"Sperman" ,edad:23, poder:"Acero",univers:"DC"},
+    {id: 12,name:"Wolverine", edad:40, poder:"Inmortal" ,univers:"MARVEL"},
+    {id: 13, name:"Hulk", edad:30, poder:"Fuerza",univers:"MARVEL"},
+    {id: 14,name:"Spider-Man", edad:44,poder:"Trepar Paredes"  ,univers:"MARVEL"},
+    {id: 15,name:"Batman", edad:34, poder:"Intelecto Nivel Genio" ,univers:"DC"},
+    {id: 16, name:"Flash",edad:19, poder:"Acero" ,univers:"DC"},
+    {id: 17,name:"Super Girl" ,edad:25, poder:"Acero" ,univers:"DC"},
+    {id: 18,name:"Bruja Scarlett" ,edad:34,poder:"Acero" ,univers:"MARVEL"},
+    {id: 19, name:"Woder Woman",edad:22, poder:"Acero" ,univers:"DC"},
+    {id: 20,name:"Thor" ,edad:1000, poder:"Acero" ,univers:"MARVEL"}
+
+]

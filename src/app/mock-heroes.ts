562ced8e145c27ec134962c571e34fe3bdc92e5b
unknown <78619347+Jovany-Vergara@users.noreply.github.com>
2021-04-12 12:07:46 -0500

    Update mock-heroes.ts

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,15 +1,15 @@
-import {Hero}from "./hero";
+import {Hero} from './hero';
 
-export const HEROES: Hero[]=[
-    {id: 11, name:"Superman" ,edad:23, poder:"Volar, Fuerza Sobrehumana, Telequinesis, Invisibilidad, Alas de Fuego",univers:"DC"},
-    {id: 12,name:"Wolverine", edad:40, poder:"Regeneracion, Garras Rectractiles" ,univers:"MARVEL"},
-    {id: 13, name:"Hulk", edad:30, poder:"Fuerza",univers:"MARVEL"},
-    {id: 14,name:"Spider-Man", edad:44,poder:"Trepar Paredes"  ,univers:"MARVEL"},
-    {id: 15,name:"Batman", edad:34, poder:"Intelecto Nivel Genio" ,univers:"DC"},
-    {id: 16, name:"Flash",edad:19, poder:"Super Velocidad" ,univers:"DC"},
-    {id: 17,name:"Super Girl" ,edad:25, poder:"Volar, Fuerza Sobrehumana, Telequinesis, Invisibilidad, Alas de Fuego" ,univers:"DC"},
-    {id: 18,name:"Scarlet Witch" ,edad:34,poder:"Proyección Astral, Control del Tiempo" ,univers:"MARVEL"},
-    {id: 19, name:"Woder Woman",edad:22, poder:"Diosa Griega" ,univers:"DC"},
-    {id: 20,name:"Thor" ,edad:1000, poder:"Dios Del Trueno" ,univers:"MARVEL"}
-
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Superman', edad: 23, poder: 'Volar, Fuerza Sobrehumana, Telequinesis, Invisibilidad, Alas de Fuego', univers: 'DC'},
+    {id: 12, name: 'Wolverine', edad: 40, poder: 'Regeneracion, Garras Rectractiles' , univers: 'MARVEL'},
+    {id: 13, name: 'Hulk', edad: 30, poder: 'Fuerza', univers: 'MARVEL'},
+    {id: 14, name: 'Spider-Man', edad: 44, poder: 'Trepar Paredes'  , univers: 'MARVEL'},
+    {id: 15, name: 'Batman', edad: 34, poder: 'Intelecto Nivel Genio' , univers: 'DC'},
+    {id: 16, name: 'Flash', edad: 19, poder: 'Super Velocidad' , univers: 'DC'},
+    {id: 17, name: 'Super Girl' , edad: 25, poder: 'Volar, Fuerza Sobrehumana, Telequinesis, Invisibilidad, Alas de Fuego' , univers: 'DC'},
+    {id: 18, name: 'Scarlet Witch' , edad: 34, poder: 'Proyección Astral, Control del Tiempo' , univers: 'MARVEL'},
+    {id: 19, name: 'Woder Woman', edad: 22, poder: 'Diosa Griega' , univers: 'DC'},
+    {id: 20, name: ' Thor', edad: 1000, poder: 'Dios Del Trueno' , univers: 'MARVEL'},
 ]
+
